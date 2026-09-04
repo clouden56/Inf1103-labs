b32b499d646538e62e2be85bd201fb26175a2093
Fadil <fadilsalleh56@gmail.com>
2026-09-04 15:09:50 +0800

    Updating profile

--- hello.py
+++ hello.py
@@ -2,3 +2,11 @@ print("==================================")
 print("Hello, World!")
 print("My first post!")
 print("==================================")
+
+username = "Fadeez"
+bio = "I am a software developer and I love coding!"
+followers = 150
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
